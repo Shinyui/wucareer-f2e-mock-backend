7ac7db0afbb627ea25b2a03d29d6e3dc552d7d5f
Shinyui Chu <shinyuichu@Shinyuis-MacBook-Pro.local>
2023-02-22 14:46:08 +0800

    add start & limit feature to get fake product data

--- src/controllers/fakeProduct.js
+++ src/controllers/fakeProduct.js
@@ -2,9 +2,12 @@ const fakeProductData = require("../models/fake-data/fakeProductData");
 const { ReasonPhrases, StatusCodes } = require("http-status-codes");
 
 const httpGetFakeProductData = (req, res) => {
-  return res
-    .status(StatusCodes.OK)
-    .send({ message: ReasonPhrases.OK, data: fakeProductData });
+  let { start, limit } = req.query;
+
+  return res.status(StatusCodes.OK).send({
+    message: ReasonPhrases.OK,
+    data: fakeProductData.slice(start, limit),
+  });
 };
 
 module.exports = {
