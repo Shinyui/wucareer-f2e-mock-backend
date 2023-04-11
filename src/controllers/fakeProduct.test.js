76630f4ef6284fa2c852aded8b4c4c87328f52fe
shinyuichu <shinyuichu325@gmail.com>
2023-04-11 14:48:42 +0800

    update test bug

--- src/controllers/fakeProduct.test.js
+++ src/controllers/fakeProduct.test.js
@@ -3,9 +3,9 @@ const { ReasonPhrases, StatusCodes } = require("http-status-codes");
 const app = require("../app");
 const fakeProductData = require("../models/fake-data/fakeProductData");
 
-describe("GET /fake/product", () => {
+describe("GET /fake/products", () => {
   it("should return all fake product data", async () => {
-    const res = await request(app).get("/fake/product");
+    const res = await request(app).get("/fake/products");
     expect(res.status).toBe(StatusCodes.OK);
     expect(res.body.message).toBe(ReasonPhrases.OK);
     expect(JSON.stringify(res.body.data)).toBe(JSON.stringify(fakeProductData));
